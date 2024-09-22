a7d847ef0ddeec9aa56d45e1ff1f09c78517dde7
Costea-B <155655464+Costea-B@users.noreply.github.com>
2024-09-22 21:43:41 +0300

    aa

---
+++ Core/Models/Requests/CreateReplyRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models.Requests
+{
+    public class CreateReplyRequest
+    {
+        public int AuthorId { get; set; }
+        public string Description { get; set; }
+        public int TopicId { get; set; }
+        public int? ParentReplyId { get; set; }
+    }
+}
